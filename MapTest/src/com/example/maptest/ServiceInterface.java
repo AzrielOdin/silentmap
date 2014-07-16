f93248b21cb0a3fe997e079da9d7303e0d4ae7f9
Andrei-Predoiu <andrei.vn92@gmail.com>
2014-07-16 14:18:29 +0200

    Added basic service interface

---
+++ MapTest/src/com/example/maptest/ServiceInterface.java
@@ -0,0 +1,23 @@
+package com.example.maptest;
+
+import java.util.ArrayList;
+
+import com.example.maptest.json.Area;
+import com.google.android.gms.maps.model.LatLng;
+
+public interface ServiceInterface {
+
+	public void setCircles();
+
+	public ArrayList<Area> getCircles();
+
+	public void setUpdateRate(int minutes);
+
+	public int getUpdateRate();
+
+	public LatLng checkCurrentPosition(); // Returns current position and
+											// updates setting accordingly
+
+	public LatLng getPosition();// Only returns current position
+
+}
